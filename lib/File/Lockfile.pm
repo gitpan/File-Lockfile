a63c5b37b0c2869f8c7c6077b744b291267c7bcb
Sergey Sinkovskiy <glorybox@cpan.org>
2008-10-13 08:21:33 +0000

    Import of GLORYBOX/File-Lockfile-1.0.0 from CPAN.
    gitpan-cpan-distribution: File-Lockfile
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         GLORYBOX/File-Lockfile-1.0.0.tar.gz
    gitpan-cpan-author:       GLORYBOX
    gitpan-cpan-maturity:     released

---
+++ lib/File/Lockfile.pm
@@ -0,0 +1,41 @@
+package File::Lockfile;
+
+use version; our $VERSION = qv('1.0.0');
+
+require Class::Data::Inheritable;
+use base qw(Class::Data::Inheritable);
+
+__PACKAGE__->mk_classdata(qw/lock_key/);
+
+sub new {
+	my ($class, $lock_key, $dir) = @_;
+	$class->lock_key($dir.'/'.$lock_key.'.lck');	
+	return bless {},$class;
+}
+
+sub write {
+	my $fh;
+	open $fh, '>', __PACKAGE__->lock_key;
+	print $fh $$;
+}
+
+sub remove {
+	unlink __PACKAGE__->lock_key;	
+}
+
+sub check {
+	my ($class, $lock_key) = @_;
+	
+	$lock_key = __PACKAGE__->lock_key unless $lock_key;
+	
+	if ( -s $lock_key ) {
+		my $fh;
+		open $fh, '<', $lock_key;
+		my $pid = <$fh>;
+		my $running = kill 0,$pid;
+		return $pid if $running;
+	}
+	return 0;
+}
+
+1;
